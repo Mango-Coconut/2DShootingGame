4a9776ee7e2bceed7c19b1baf55c941e0af384ee
OhSeungSe <39735663+Mango-Coconut@users.noreply.github.com>
2025-08-07 22:39:01 +0900

    refactor: add SOLID-based game skeleton

--- Assets/0. Script/ObjectPool.cs
+++ Assets/0. Script/ObjectPool.cs
@@ -1,11 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
+public class ObjectPool<T> where T : Component
 {
-    //Pooling PlayerBullet
+    private readonly T prefab;
+    private readonly Queue<T> objects = new Queue<T>();
 
-    //Pooling EnemyBullet
-    
+    public ObjectPool(T prefab, int initialSize)
+    {
+        this.prefab = prefab;
+        for (int i = 0; i < initialSize; i++)
+        {
+            T obj = Object.Instantiate(prefab);
+            obj.gameObject.SetActive(false);
+            objects.Enqueue(obj);
+        }
+    }
+
+    public T Get()
+    {
+        if (objects.Count == 0)
+        {
+            T obj = Object.Instantiate(prefab);
+            obj.gameObject.SetActive(false);
+            objects.Enqueue(obj);
+        }
+
+        T instance = objects.Dequeue();
+        instance.gameObject.SetActive(true);
+        return instance;
+    }
+
+    public void Return(T obj)
+    {
+        obj.gameObject.SetActive(false);
+        objects.Enqueue(obj);
+    }
 }
